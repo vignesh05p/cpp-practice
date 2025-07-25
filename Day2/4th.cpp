bebdf07e1e34f464356ee3de85449593db1023db
vignesh05p <vigneshprabhu050@gmial.com>
2025-07-25 19:21:20 +0530

    4th question

---
+++ Day2/4th.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cctype> 
+
+using namespace std;
+
+int main() {
+    double temp;
+    char unit;
+
+    cout << "************************************" << endl;
+    cout << "******** Temperature Converter ******" << endl;
+
+    cout << "Enter unit (C for Celsius / F for Fahrenheit): ";
+    cin >> unit;
+
+    unit = tolower(unit); 
+
+    if (unit != 'c' && unit != 'f') {
+        cout << "|| Enter a valid unit (C or F) ||" << endl;
+        return 1;
+    }
+
+    cout << "Enter Temperature: ";
+    cin >> temp;
+
+    if (unit == 'c') {
+        double fahrenheit = (temp * 1.8) + 32;
+        cout << "Temperature in Fahrenheit: " << fahrenheit << "°F" << endl;
+    } else {
+        double celsius = (temp - 32) / 1.8;
+        cout << "Temperature in Celsius: " << celsius << "°C" << endl;
+    }
+
+    return 0;
+}
