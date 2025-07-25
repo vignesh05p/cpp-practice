2ca9b4db72e2b8f74bcb3e6f87b12afd8a477719
vignesh05p <vigneshprabhu050@gmial.com>
2025-07-25 15:04:58 +0530

    2nd question

---
+++ Day2/2nd.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+int main(){
+    double a;
+    double b;
+    double c;
+    
+   std::cout << "enter the a" << std::endl;
+   std::cin >> a;
+   
+    std::cout << "enter the b" << std::endl;
+   std::cin >> b;
+   
+   c=sqrt(pow(a,2)+(pow(b,2)));
+   
+   std::cout<<c;
+    
+    
+    
+    return 0;
+}
