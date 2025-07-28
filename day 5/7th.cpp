f4b53f49b63a8d8fabca640adf820c42a45f6ca0
vignesh05p <vigneshprabhu050@gmail.com>
2025-07-28 20:43:48 +0530

    completed 8th question

--- day 5/7th.cpp
+++ day 5/7th.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
+#include <cmath> // For abs()
 using namespace std;
 
 int main() {
     int num, count = 0;
-    cout << "Enter a number: ";
+
+    cout << "Enter an integer: ";
     cin >> num;
 
-    // Handle 0 explicitly (0 has 1 digit)
+    // Input validation: check if the user entered an integer
+    if (cin.fail()) {
+        cout << "Invalid input. Please enter a valid integer." << endl;
+        return 1;
+    }
+
+    // Handle 0 explicitly
     if (num == 0) {
         count = 1;
     } else {
-        // Take absolute in case of negative numbers
-        num = abs(num);
-        while (num > 0) {
-            num /= 10; // Remove last digit
+        int temp = abs(num); // Handle negative numbers
+
+        while (temp > 0) {
+            temp /= 10;
             ++count;
         }
     }
 
-    cout << "Number of digits = " << count << endl;
+    cout << "Number of digits in " << num << " is: " << count << endl;
 
     return 0;
 }
