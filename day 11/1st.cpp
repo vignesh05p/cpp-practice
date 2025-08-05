b64e1261ebf63449e9c106ebc670690586a8e1ee
vignesh05p <vigneshprabhu050@gmail.com>
2025-08-05 21:48:43 +0530

    day 11

---
+++ day 11/1st.cpp
@@ -0,0 +1,14 @@
+// 💡 Problem: Validate Credit Card Number Using Luhn Algorithm
+// Problem Statement:
+
+// Write a function that checks whether a given credit card number is valid using the Luhn Algorithm.
+
+// The Luhn algorithm works as follows:
+
+// Starting from the right, double every second digit (i.e., digit at positions -2, -4, etc.).
+
+// If the doubled value is greater than 9, subtract 9 from it.
+
+// Sum all the digits (both modified and untouched).
+
+// If the total modulo 10 is 0, the card number is valid
